cd853fc5ac5b879b6a697a30ccb3fa3f76d27adc
unknown <k.prakash210012@gmail.com>
2023-10-05 21:52:30 +0530

    Project Done

--- login_signup/src/App.js
+++ login_signup/src/App.js
@@ -1,34 +1,111 @@
 import "./App.css";
+import { getAuth,signInWithPopup,GoogleAuthProvider,signInWithEmailAndPassword,createUserWithEmailAndPassword,sendPasswordResetEmail } from "firebase/auth";
 import {useState} from "react";
+import app  from "./firebase";
+
+
+
 function App() {
+  const handleGoogleLogin = async()=>{
+    const provider = new GoogleAuthProvider()
+    const result = await signInWithPopup(auth, provider).then((result)=>{
+      alert("Google Login Successful")
+  })
+  .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+          });
+  }
+
+  const auth = getAuth(app);
+  const [email,setEmail] = useState("")
+  const [password,setpassword] = useState("")
+  const sign_up = () =>{
+
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed up 
+          const user = userCredential.user;
+          alert("account created")
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          
+          // ..
+        });
+  }
+
+  const login = () =>{
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            alert("Login successful")
+            // ...
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+          });
+}
+
+
+sendPasswordResetEmail(auth,  email)
+  .then(() => {
+    // Password reset email sent!
+    alert("password reset mail sent")
+    // ..
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert("Enter email")
+    
+  //   // ..
+  });
+
   const [action, setAction] = useState("Login");
   return (
     <>
+    
       <div className="block">
         <div className="section">
           <div className="Header">
             <div className="heading">{action}</div>
             <div className="line"></div>
+            
           </div>
         </div>
         <div className="inputs">
-          {action === "Login"?<div/>:<div className="form">
+          {action === "Login" || "Password Reset"?<div/>:<div className="form">
             <input type="text" placeholder="Name" />
           </div>}
           
           <div className="form">
-            <input type="email" placeholder="Email" />
+            <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
           </div>
-          <div className="form">
-            <input type="password" placeholder="Password" />
-          </div>
-          {action === "Sign Up"?<div/>:<div className="reset">
-            Forgot password? <span>Click Here</span>
+          
+          {action === "Password Reset"?<div/>:<div className="form">
+            <input type="password" placeholder="Password" onChange={(e) => setpassword(e.target.value)}/>
           </div>}
+
+          {action === "SignUp"?<div/>:<div className="reset" onClick={()=>{setAction("Password Reset")}}>
+            Forgot password? <span onClick={sendPasswordResetEmail}>Click Here</span>
+          </div>}
+
           
           <div className="submit">
-            <div className="signup" onClick={()=>{setAction("Sign Up")}}>Signup</div>
-            <div className="signup" onClick={()=>{setAction("Login")}}>Login</div>
+            <div className="Google">
+            <button onClick={handleGoogleLogin}>Sign In with google</button>
+            </div>
+            <div className="signup" onClick={()=>{setAction("SignUp")}}>
+            <button onClick={sign_up}>SIgnup</button></div>
+            <div className="signup" onClick={()=>{setAction("Login")}}>
+            <button onClick={login}>Login</button>
+            </div>
           </div>
         </div>
       </div>
--- login_signup/src/firebase.js
+++ login_signup/src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBa1Fg3pXdxKLW_0mZtgsLsBfvNlFQHGBw",
@@ -13,5 +13,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+export default app;
